24fa82e3d4aae4b22c4533b9efc60faf86fad2f8
SLAYSENFITE <wessels.sm@gmail.com>
2017-07-17 18:13:05 +0200

    Added Supplier, Product page

--- app/src/main/java/com/projects/wesse/apollo_ui/Products.java
+++ app/src/main/java/com/projects/wesse/apollo_ui/Products.java
@@ -3,15 +3,19 @@ package com.projects.wesse.apollo_ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
-public class Products extends AppCompatActivity {
+public class Products extends AppCompatActivity{
 
     ArrayList<String> products;
     ArrayAdapter<String> adapter;
@@ -22,6 +26,7 @@ public class Products extends AppCompatActivity {
         setContentView(R.layout.activity_products);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,4 +39,5 @@ public class Products extends AppCompatActivity {
 
         Intent previousActivity = getIntent();
     }
+
 }

b8b360f930ce21fb0726d3ab310f996997727a81
Xander <xanderferreira@yahoo.com>
2017-07-19 11:17:09 +0200

    Products list fixed

---
+++ app/src/main/java/com/projects/wesse/apollo_ui/RestClientUsage.java
@@ -0,0 +1,8 @@
+package com.projects.wesse.apollo_ui;
+
+/**
+ * Created by Xander on 7/19/2017.
+ */
+
+public class RestClientUsage {
+}
---
+++ app/src/main/java/com/projects/wesse/apollo_ui/utilities/RestClient.java
@@ -0,0 +1,8 @@
+package com.projects.wesse.apollo_ui.utilities;
+
+/**
+ * Created by Xander on 7/19/2017.
+ */
+
+public class RestClient {
+}

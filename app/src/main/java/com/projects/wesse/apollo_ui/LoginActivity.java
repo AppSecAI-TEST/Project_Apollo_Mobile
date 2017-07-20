26fa857423781d6cd120572d7f84039054a6e1ca
SLAYSENFITE <wessels.sm@gmail.com>
2017-07-20 17:52:48 +0200

    Changed aesthetics

--- app/src/main/java/com/projects/wesse/apollo_ui/LoginActivity.java
+++ app/src/main/java/com/projects/wesse/apollo_ui/LoginActivity.java
@@ -299,8 +299,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                //finish();
                 loginAction();
+                //finish();
+
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();

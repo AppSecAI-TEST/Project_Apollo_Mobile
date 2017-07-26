113e89ac9dbda684cfaf0f001fb1fc111c332c48
Xander <xanderferreira@yahoo.com>
2017-07-26 18:36:19 +0200

    Improved listView actions

---
+++ app/src/main/java/com/projects/wesse/apollo_ui/MenuTest.java
@@ -0,0 +1,31 @@
+package com.projects.wesse.apollo_ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Xander on 7/25/2017.
+ */
+
+public class Menu extends Fragment {
+
+
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.activity_products, container, false);
+    }
+
+
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Menu 1");
+    }
+}

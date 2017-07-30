a1c5182fc5a05305249479107d5f2ce373af6557
Xander <xanderferreira@yahoo.com>
2017-07-30 16:47:58 +0200

    Graph changes

--- app/src/main/java/com/projects/wesse/apollo_ui/Dashboard.java
+++ app/src/main/java/com/projects/wesse/apollo_ui/Dashboard.java
@@ -1,15 +1,37 @@
 package com.projects.wesse.apollo_ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.projects.wesse.apollo_ui.utilities.BaseActivity;
 
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+
 public class Dashboard extends BaseActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,27 +40,85 @@ public class Dashboard extends BaseActivity {
         super.onCreateDrawer();
 
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        graph.getViewport().setScrollable(true);
-        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-        staticLabelsFormatter.setHorizontalLabels(new String[] {"Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec", "Jan"});
-        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
 
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(1, 6500),
-                new DataPoint(2, 5900),
-                new DataPoint(3, 8000),
-                new DataPoint(4, 8100),
-                new DataPoint(5, 5600),
-                new DataPoint(6, 5500),
-                new DataPoint(7, 4000),
-                new DataPoint(8, 6500),
-                new DataPoint(9, 5900),
-                new DataPoint(10, 8000),
-                new DataPoint(11, 8100),
-                new DataPoint(12, 5600),
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+
+
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 6500));
+        entries.add(new Entry(2, 7800));
+        entries.add(new Entry(3, 3200));
+        entries.add(new Entry(4, 4500));
+        entries.add(new Entry(5, 1300));
+        entries.add(new Entry(6, 6500));
+        entries.add(new Entry(7, 7800));
+        entries.add(new Entry(8, 3200));
+        entries.add(new Entry(9, 4500));
+        entries.add(new Entry(10, 1300));
+        entries.add(new Entry(11, 4500));
+        entries.add(new Entry(12, 1300));
+
+        final ArrayList<String> xLabel = new ArrayList<String>();
+        xLabel.add("");
+        xLabel.add("January");
+        xLabel.add("February");
+        xLabel.add("March");
+        xLabel.add("April");
+        xLabel.add("May");
+        xLabel.add("June");
+        xLabel.add("July");
+        xLabel.add("August");
+        xLabel.add("September");
+        xLabel.add("October");
+        xLabel.add("November");
+        xLabel.add("December");
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return xLabel.get((int)value);
+            }
         });
-        graph.addSeries(series);
+
+        YAxis rightYAxis = chart.getAxisRight();
+        rightYAxis.setEnabled(false);
+
+        LineDataSet dataSet = new LineDataSet(entries, "Stock"); // add entries to dataset
+        dataSet.setDrawValues(false);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setDrawGridBackground(false);
+        chart.getDescription().setEnabled(false);
+
+        chart.setNoDataText("No input found");
+
+        chart.setData(lineData);
+        chart.invalidate();
+
+//        GraphView graph = (GraphView) findViewById(R.id.graph);
+//        graph.getViewport().setScrollable(true);
+//        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+//        staticLabelsFormatter.setHorizontalLabels(new String[] {"Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec", "Jan"});
+//        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+//
+//        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+//                new DataPoint(1, 6500),
+//                new DataPoint(2, 5900),
+//                new DataPoint(3, 8000),
+//                new DataPoint(4, 8100),
+//                new DataPoint(5, 5600),
+//                new DataPoint(6, 5500),
+//                new DataPoint(7, 4000),
+//                new DataPoint(8, 6500),
+//                new DataPoint(9, 5900),
+//                new DataPoint(10, 8000),
+//                new DataPoint(11, 8100),
+//                new DataPoint(12, 5600),
+//        });
+//        graph.addSeries(series);
 
 
 
@@ -47,6 +127,8 @@ public class Dashboard extends BaseActivity {
         Intent previousActivity = getIntent();
         String dataFromPrev = previousActivity.getExtras().getString("fromLogin");
     }
+
+
 
 
 

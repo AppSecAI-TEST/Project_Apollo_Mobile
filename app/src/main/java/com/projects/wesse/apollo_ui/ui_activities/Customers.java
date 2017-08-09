bbd71e39b146d7a9edc3773f74c11d6e1f9e06c8
Xander <xanderferreira@yahoo.com>
2017-08-09 19:49:31 +0200

    Added pagination

--- app/src/main/java/com/projects/wesse/apollo_ui/ui_activities/Customers.java
+++ app/src/main/java/com/projects/wesse/apollo_ui/ui_activities/Customers.java
@@ -8,8 +8,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.app.ActionBar.LayoutParams;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.projects.wesse.apollo_ui.Attributes.Customer;
 import com.projects.wesse.apollo_ui.R;
@@ -29,6 +33,14 @@ public class Customers extends BaseActivity {
 
     private ArrayList<Customer> allCustomers, shownCustomers;
     private ArrayList<String> cust_names;
+    ListView list_customers;
+    ArrayAdapter<String> adapter;
+
+    public int TOTAL_LIST_ITEMS;
+    public int NUM_ITEMS_PAGE = 10;
+    int currentPage = 0;
+    private int noOfBtns;
+    private Button[] btns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +74,8 @@ public class Customers extends BaseActivity {
             }
         } catch (JSONException e) {e.printStackTrace();}
 
+        TOTAL_LIST_ITEMS = allCustomers.size();
+
         shownCustomers = new ArrayList<Customer>();
         cust_names = new ArrayList<String>();
         for(int i = 0; i < allCustomers.size(); i++)
@@ -70,30 +84,38 @@ public class Customers extends BaseActivity {
         }
         loadMoreData(shownCustomers.size());
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cust_names);
-        ListView list_customers = (ListView) findViewById(R.id.listView1);
+        list_customers = (ListView) findViewById(R.id.listView1);
+
+        Btnfooter();
+
+        loadList(0);
+
+        CheckBtnBackGroud(0);
+
+//        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cust_names);
+//        ListView list_customers = (ListView) findViewById(R.id.listView1);
 
         Button btnLoadMore = new Button(this);
-        btnLoadMore.setText("Load More");
+//        btnLoadMore.setText("Load More");
 
         // Adding Load More button to lisview at bottom
-        list_customers.addFooterView(btnLoadMore);
+//        list_customers.addFooterView(btnLoadMore);
         list_customers.setAdapter(adapter);
 
-        btnLoadMore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                // Starting a new async task
-                loadMoreData(shownCustomers.size());
-                adapter.notifyDataSetChanged();
-            }
-        });
+//        btnLoadMore.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View arg0) {
+//                // Starting a new async task
+//                loadMoreData(shownCustomers.size());
+//                adapter.notifyDataSetChanged();
+//            }
+//        });
 
         list_customers.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent cust_view = new Intent(view.getContext(), CustomerView.class).putExtra("CUST", (Serializable) allCustomers.get((int) id));
-                startActivity(cust_view);
+                Intent product_view = new Intent(view.getContext(), CustomerView.class).putExtra("CUST", (Serializable) allCustomers.get((int)id + (10*currentPage)));
+                startActivity(product_view);
             }
 
 
@@ -117,6 +139,79 @@ public class Customers extends BaseActivity {
         for (int i = length ; i < length + 10; i++)
             if(allCustomers.size() > i)
                 shownCustomers.add(allCustomers.get(i));
+    }
+
+    private void Btnfooter()
+    {
+        int val = TOTAL_LIST_ITEMS%NUM_ITEMS_PAGE;
+        val = val==0?0:1;
+        noOfBtns=TOTAL_LIST_ITEMS/NUM_ITEMS_PAGE+val;
+
+        LinearLayout ll = (LinearLayout)findViewById(R.id.btnLay);
+
+        btns    =new Button[noOfBtns];
+
+        for(int i=0;i<noOfBtns;i++)
+        {
+            btns[i] =   new Button(this);
+            btns[i].setBackgroundColor(getResources().getColor(android.R.color.transparent));
+            btns[i].setText(""+(i+1));
+
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            ll.addView(btns[i], lp);
+
+            final int j = i;
+            btns[j].setOnClickListener(new OnClickListener() {
+
+                public void onClick(View v)
+                {
+                    currentPage = j;
+                    loadList(j);
+                    CheckBtnBackGroud(j);
+                }
+            });
+        }
+
+    }
+
+    private void CheckBtnBackGroud(int index)
+    {
+//        title.setText("Page "+(index+1)+" of "+noOfBtns);
+        for(int i=0;i<noOfBtns;i++)
+        {
+            if(i==index)
+            {
+                btns[index].setBackgroundDrawable(getResources().getDrawable(R.drawable.common_google_signin_btn_icon_dark_normal));
+                btns[i].setTextColor(getResources().getColor(android.R.color.white));
+            }
+            else
+            {
+                btns[i].setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                btns[i].setTextColor(getResources().getColor(android.R.color.black));
+            }
+        }
+    }
+
+    private void loadList(int number)
+    {
+        ArrayList<String> sort = new ArrayList<String>();
+
+        int start = number * NUM_ITEMS_PAGE;
+        for(int i=start;i<(start)+NUM_ITEMS_PAGE;i++)
+        {
+            if(i<allCustomers.size())
+            {
+                sort.add(allCustomers.get(i).getName());
+            }
+            else
+            {
+                break;
+            }
+        }
+        adapter= new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                sort);
+        list_customers.setAdapter(adapter);
     }
 
 }
